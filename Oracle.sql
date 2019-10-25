434ad2969742051b8ccfc02402f72b625916d584
Uğur KILIÇ <ugurkilic91@gmail.com>
2019-10-25 11:43:23 +0300

    ekleme

--- Oracle.sql
+++ Oracle.sql
@@ -12,3 +12,14 @@ ORDER BY cols.table_name, cols.position;
 
 --Veritabanında bulunan tüm sequence listeler
 select sequence_owner, sequence_name from dba_sequences t where t.sequence_owner='Şema';
+
+-- Tablo, view, sequence gibi objelerin oluşturulma ve son güncelleme tarihleri getirilecektir.
+select object_name, object_type, created, last_ddl_time 
+from user_objects
+where object_name = 'OBJE_ADI';
+
+-Tablo yada view unique kontrolu, belirtilen tabloda ve kolona göre unique olmayan kayıt varsa getirir.
+SELECT kolon_adi, COUNT(kolon_adi)
+FROM Tablo_adi
+GROUP BY kolon_adi
+HAVING COUNT(kolon_adi) > 1;
