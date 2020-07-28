fb26d19bdce9ade0628b2ec0250f73cb49b92ec1
Uğur KILIÇ <ugurkilic91@gmail.com>
2020-07-28 14:55:24 +0300

    Update Oracle.sql

--- Oracle.sql
+++ Oracle.sql
@@ -1,3 +1,16 @@
+-- Fk(constraint_name) göre bağlı olduğu tabloyu gösterir.
+sELECT a.table_name, a.column_name, a.constraint_name, c.owner, 
+       -- referenced pk
+       c.r_owner, c_pk.table_name r_table_name, c_pk.constraint_name r_pk
+  FROM all_cons_columns a
+  JOIN all_constraints c ON a.owner = c.owner
+                        AND a.constraint_name = c.constraint_name
+  JOIN all_constraints c_pk ON c.r_owner = c_pk.owner
+                           AND c.r_constraint_name = c_pk.constraint_name
+ WHERE c.constraint_type = 'R'
+   AND a.constraint_name = 'FK_ENT_ENTCALISMA'
+
+
 --Veritabanında bulunan tablolarda primary key(PK) alanları listeler.
 SELECT cols.table_name, cols.column_name, cols.position, cons.status, cons.owner
 FROM all_constraints cons, all_cons_columns cols
